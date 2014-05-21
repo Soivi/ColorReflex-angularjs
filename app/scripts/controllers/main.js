54e54ae6ef76ef79c631bec344fbe83440cde4dc
Live session user <xubuntu@xubuntu.(none)>
2014-05-21 11:31:24 +0000

    yo angular project added

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('colorReflexAngularjsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
