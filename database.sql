0fe5b4b0a5e1ca263802d3250524ca64ab2d12ff
Lauri Soivi <soivi.lauri@gmail.com>
2014-05-25 16:21:13 +0300

    database and highscore.php added

---
+++ database.sql
@@ -0,0 +1,14 @@
+# HighscoresTable
+CREATE TABLE highscores (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(100), time DOUBLE);
+
+# TestData
+INSERT INTO highscores (name,time) VALUES('Jane', 100.89);
+INSERT INTO highscores (name,time) VALUES('Jill', 110);
+INSERT INTO highscores (name,time) VALUES('Walter', 115.9);
+INSERT INTO highscores (name,time) VALUES('Eve', 135.42);
+INSERT INTO highscores (name,time) VALUES('John', 143.6);
+INSERT INTO highscores (name,time) VALUES('Adam', 146.49);
+INSERT INTO highscores (name,time) VALUES('Aden', 157.12);
+INSERT INTO highscores (name,time) VALUES('Steven', 240.85);
+INSERT INTO highscores (name,time) VALUES('Santos', 156.43);
+INSERT INTO highscores (name,time) VALUES('Alexis', 277.57);
